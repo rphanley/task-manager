0b2781da6c314307028b0fd347ad0bd916b60f0d
rphanley <rphanley@rocketmail.com>
2019-12-05 21:01:40 +0000

    Completed insert task form

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb://root:r00tUser@myfirstcluster-shard-00-00-tlnzz.mongodb.net:27017,myfirstcluster-shard-00-01-tlnzz.mongodb.net:27017,myfirstcluster-shard-00-02-tlnzz.mongodb.net:27017/task_manager?ssl=true&replicaSet=myFirstCluster-shard-0&authSource=admin&retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World..again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html', categories=mongo.db.categories.find())
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
